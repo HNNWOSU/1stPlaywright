9fa4101740a4806f129a3df53b80035ee57609d6
Henry Nwosu <>
2024-05-27 20:33:30 +0100

    Hnn Tau playwright commit

---
+++ tests/UIBasicTest.spec.js
@@ -0,0 +1,24 @@
+const {test, expect} = require('@playwright/test');
+
+
+
+test('Browser Context Playwright Test', async ({browser})=>
+{
+
+ const context = await browser.newContext();
+ const page = await context.newPage();
+  await page.goto ("https://rahulshettyacademy.com/loginpagePractise/");
+
+
+});
+
+
+test ('Page Playwright Test', async ({page})=>
+    {
+    
+      await page.goto ("https://google.com");
+    console.log(await page.title());
+    await expect (page) . toHaveTitle("Google");
+
+
+    });
